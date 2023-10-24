18f43ec4192c73960585d4f3ef4a776b70e27b01
Ibrahimkhatuda <125338151+Ibrahimkhatuda@users.noreply.github.com>
2023-10-24 10:38:54 +0500

    changes

--- Eproject/admin/branchadd.php
+++ Eproject/admin/branchadd.php
@@ -3,9 +3,9 @@ include('connection.php');
 $select_q ='SELECT * FROM `tbl_branch`';
 $run_q = mysqli_query($conn, $select_q);
 
- 
-    
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,6 +17,7 @@ $run_q = mysqli_query($conn, $select_q);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <title>SB Admin 2 - Dashboard</title>
 
@@ -57,56 +58,57 @@ $run_q = mysqli_query($conn, $select_q);
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
+                 
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">View Products</h1>
-                        <a href="maintainproducts.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"> Add Product</a>
+                        <h1 class="h3 mb-0 text-gray-800">Maintain Products</h1>
+                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
 
-<div class="container">
-<table class="table table-bordered">
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Name</th>
-      <th>Description</th>
-      <th>Price</th>
-      <th>Images</th>
-      <th>Edit</th>
-      <th>Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php while($row = mysqli_fetch_array($run_q)){?>
-   
-    <tr>
-    <td><?php echo $row['p_id'];?></td>
-      <td><?php echo $row['p_name'];?></td>
-      <td><?php echo $row['p_des'];?></td>
-      <td><?php echo $row['p_price'];?></td>
+                
+                <!-- /.container-fluid -->
 
-      <td><img src="<?php echo $row['p_img'];?>" width="50" height="50" alt=""></td>
-      <div class="mb-3">
- 
+            </div>
+           <div class="container">
+            <form method="POST" enctype="multipart/form-data">
+  <div class="mb-3">
+    <label  class="form-label">Product Name</label>
+    <input type="text" class="form-control" name="p_name">
+  
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Description</label>
+    <input type="text" class="form-control" name="p_des">
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Price</label>
+    <input type="text" class="form-control" name="p_price">
+    
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Image</label>
+      <img src="<?php echo $row['p_img'];?>" width="50" height="50" alt="">
+    <input type="file" class="form-control" name="p_img"> 
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Category</label>
+    
+    <select class="form-control" name="c_name">
+   <?php while ($row = mysqli_fetch_array($run_q)) { ?>
+      <option value="<?php echo $row['c_id']; ?>"><?php echo $row['c_name']; ?></option>
+
+   <?php } ?>
+</select>
 
 
     
   </div>
-      <td><a href="editproduct.php?id=<?php echo $row['p_id'];?>" class="btn btn-success">Edit</a></td>
-      <td><a href="deleteproduct.php?id=<?php echo $row['p_id'];?>" class="btn btn-danger">Delete</a></td>
-    
-
-    </tr>
-  <?php } ?>
-  </tbody>
-</table>
-</div>
-                </div>
-                <!-- /.container-fluid -->
-
-            </div>
+  <button type="submit" class="btn btn-primary" name="btnsubmit">Submit</button>
+</form>
+  </div>
             <!-- End of Main Content -->
 
             <!-- Footer -->
