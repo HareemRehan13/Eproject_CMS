ee8247766544b5c8798e7d0c80d04b7fb728b34b
Ibrahimkhatuda <125338151+Ibrahimkhatuda@users.noreply.github.com>
2023-11-04 10:48:43 +0500

    gsdgsg

--- Eproject/user/about.php
+++ Eproject/user/about.php
@@ -1,3 +1,8 @@
+<?php
+include('connection.php');
+$fetch_q = "SELECT * FROM `tbl_company`";
+$run_q = mysqli_query($conn, $fetch_q);
+?>
  <!-- ======= Header ======= -->
   <?php include('navbar.php');?>
   <!-- End Header -->
@@ -110,6 +115,7 @@
 
     <!-- ======= Our Team Section ======= -->
     <section id="team" class="team pt-0">
+   
       <div class="container" data-aos="fade-up">
 
         <div class="section-header">
@@ -117,17 +123,18 @@
           <h2>Our Team</h2>
 
         </div>
-
         <div class="row" data-aos="fade-up" data-aos-delay="100">
+   <?php while($row = mysqli_fetch_array($run_q)){ ?>
+      
 
           <div class="col-lg-4 col-md-6 d-flex">
             <div class="member">
               <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
               <div class="member-content">
-                <h4>Walter White</h4>
-                <span>Web Development</span>
+                <h4><?php echo $row['co_name']; ?></h4>
+              
                 <p>
-                  Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut
+                <?php echo $row['co_desc']; ?>
                 </p>
                 <div class="social">
                   <a href=""><i class="bi bi-twitter"></i></a>
@@ -139,47 +146,12 @@
             </div>
           </div><!-- End Team Member -->
 
-          <div class="col-lg-4 col-md-6 d-flex">
-            <div class="member">
-              <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
-              <div class="member-content">
-                <h4>Sarah Jhinson</h4>
-                <span>Marketing</span>
-                <p>
-                  Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus
-                </p>
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div><!-- End Team Member -->
-
-          <div class="col-lg-4 col-md-6 d-flex">
-            <div class="member">
-              <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
-              <div class="member-content">
-                <h4>William Anderson</h4>
-                <span>Content</span>
-                <p>
-                  Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara
-                </p>
-                <div class="social">
-                  <a href=""><i class="bi bi-twitter"></i></a>
-                  <a href=""><i class="bi bi-facebook"></i></a>
-                  <a href=""><i class="bi bi-instagram"></i></a>
-                  <a href=""><i class="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div><!-- End Team Member -->
-
-        </div>
-
+          
+       
+         <?php } ?>
+         </div> 
       </div>
+    
     </section><!-- End Our Team Section -->
 
     <!-- ======= Testimonials Section ======= -->
