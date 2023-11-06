995a9fb007d7485e88c98adbcf5c8378d90428a4
Ibrahimkhatuda <125338151+Ibrahimkhatuda@users.noreply.github.com>
2023-11-06 22:31:57 +0500

    gdecJS

--- Eproject/user/calpricing.php
+++ Eproject/user/calpricing.php
@@ -16,34 +16,61 @@ body {
 }
 
 .calculator {
-    margin: 20px;
+  margin-left: 0px !important;
+    margin:50px;
     padding: 20px;
-    border: 1px solid #ccc;
+    border: 1px solid white;
     border-radius: 5px;
-    background-color: #f7f7f7;
     display: inline-block;
 }
 
 button {
     padding: 10px 20px;
-    background-color:black;
-    color: bisque; 
+    background-color:white;
+    color:black; 
     border: none;
     border-radius: 5px;
     cursor: pointer;
-}
 
-button:hover {
-    background-color:darkblue;
 }
 h1{
+  color: white;
   margin-top:50px;
-
+  align-items: center;
+vertical-align: middle;
 }
 .div1{
   text-align: center;
   vertical-align:middle;
   justify-content: center;
+  margin-top:80px;
+  font-size: xx-large;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-weight: bold;
+}
+.input1{
+width:270px;
+text-align: center;
+}
+.select1{
+  width:270px;
+  text-align: center;
+}
+.select2{
+  width:270px;
+  text-align: center;
+  line-height: normal;
+
+}
+.divcon{
+  background-color:#0e1d34; 
+
+}
+.div2{
+  background-color: #0e1d34;
+}
+.li{
+  color:#0e1d34;
 }
 </style>
 
@@ -54,8 +81,8 @@ h1{
         <div class="container position-relative">
           <div class="row d-flex justify-content-center">
             <div class="col-lg-6 text-center">
-              <h2>Contact</h2>
-              <p>Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+              <h2>Calculator</h2>
+              <p> Powerful automatic pricing calculator for courier. Transparency is the key to building trust in courier services </p>
             </div>
           </div>
         </div>
@@ -63,8 +90,8 @@ h1{
       <nav>
         <div class="container">
           <ol>
-            <li><a href="index.html">Home</a></li>
-            <li>Contact</li>
+            <li class="li"><a  href="index.html">Home</a></li>
+            <li class="li">Contact</li>
           </ol>
         </div>
       </nav>
@@ -96,31 +123,36 @@ else{
 }
   }
   ?>   
-<div class="container">
+<div class="container divcon ">
   <div class="row">
-    <div class="col-lg-4 div1">
-    <h4>Courier Price Calculator</h4>
+    <div class="col-lg-6 div1 ">
+    <h1>Courier Price Calculator</h1>
 </div>
-    <div class="col-lg-6">
+    <div class="col-lg-6 div2">
     <div class="calculator">
       <form action="" method="POST">
-        <input type="number" id="weight" name="weight" placeholder="Enter Weight">
-        <select id="unit" name="unit">
-            <option value="kg">Kilograms (kg)</option>
+        <input class="input1"  type="number" id="weight" name="weight" placeholder="Enter Weight">
+        <br>
+        <br>
+        <select class="select1" id="unit" name="unit">
+            <option  value="kg">Kilograms (kg)</option>
             <option value="gm">Grams (gm)</option>
         </select>
-        <select name="comp">
+        <br>
+        <br>
+        <select class="select2" name="comp">
         <?php
         $q="select * from tbl_company";
         $res=mysqli_query($conn,$q);
         while($row=mysqli_fetch_array($res)){
-        
         ?>
 <option value="<?php echo $row['co_id']?>"><?php echo $row['co_name']?></option>
         <?php
         }
         ?>
         </select>
+        <br>
+        <br>
         <button type="submit" name="calc">Calculate Price</button>
         </form>
         <p id="result"></p>
@@ -131,7 +163,7 @@ else{
     <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
 
-        <div>
+        <div class="col-lg-12">
           <iframe style="border:0; width: 100%; height: 340px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
         </div><!-- End Google Maps -->
 
